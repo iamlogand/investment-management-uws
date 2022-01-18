78cb8b462b614037588f80f2e70604f1a1fc3da2
iamlogan <iamlogandavidson@gmail.com>
2022-01-18 22:04:18 +0000

    Updated Bootstrap

--- portfoliosite/urls.py
+++ portfoliosite/urls.py
@@ -17,3 +17,18 @@ urlpatterns = [
     # URLs for allauth (login, sign up, verify email, etc.)
     path('accounts/', include('allauth.urls')),
 ]
+
+'''
+allauth templates and corresponding URLs:
+
+account_inactive.html               /accounts/inactive
+email_confirm.html	                /accounts/confirm-email/
+login.html	                        /accounts/login/
+logout.html	                        /accounts/logout/
+password_reset.html	                /accounts/password/reset/
+password_reset_done.html	        /accounts/password/reset/done/
+password_reset_from_key.html	    /accounts/password/reset/key/<key>/
+password_reset_from_key_done.html   /accounts/password/reset/key/done/
+signup.html	                        /accounts/signup/
+verification_sent.html	            /accounts/confirm-email/
+'''
